caace77b4ddc115b8f139dc23831fa64156d59af
Marvin Gajek <Marvin.Gajek1@web.de>
2024-03-31 19:07:06 +0200

    return list of deployed container registry names

--- devops/terraform/output.tf
+++ devops/terraform/output.tf
@@ -1,18 +1,4 @@
-# # Azure Storage Account service
-# output "storage_account_name_list" {
-#     value = [for storage_account in azurerm_storage_account.this : storage_account.name]
-# }
-
-# output "storage_account_primary_connection_string_list" {
-#     value = [for storage_account in azurerm_storage_account.this : storage_account.primary_connection_string]
-#     sensitive = true
-# }
-
-# output "storage_account_primary_access_key_list" {
-#     value = [for storage_account in azurerm_storage_account.this : storage_account.primary_access_key]
-#     sensitive = true
-# }
-
-# output "storage_account_container_name_list" {
-#     value = [for storage_account_container in azurerm_storage_container.this : storage_account_container.name]
-# }
+# Azure Container registry
+output "container_registry_name_list" {
+    value = [for container_registry in azurerm_container_registry.this : container_registry.name]
+}
