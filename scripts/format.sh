c545190b8bc15d50ab995cf49f0fc51e8c73f4ee
Marvin Gajek <Marvin.Gajek1@web.de>
2024-04-01 20:26:55 +0200

    add format.sh script

---
+++ scripts/format.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+set -euo pipefail
+
+SCRIPT_DIR=$(dirname "$BASH_SOURCE")
+ROOT_PROJECT_DIR=$SCRIPT_DIR/../..
+
+cd $ROOT_PROJECT_DIR
+
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+echo "#####################################################################################################"
+echo -e "$BLUE INFO: $NC About to generate auto-format hcl files and hcl docs"
+
+DEVOPS_TF_FOLDER="./devops/terraform"
+for dir in ${DEVOPS_TF_FOLDER} ; do
+  terraform-docs markdown table --output-file README.md --hide providers --output-mode replace "$dir"
+done
+
+terraform fmt -recursive .
+
+cd -
